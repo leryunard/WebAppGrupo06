faef77002ac35916ed05c4a448906e8e3869ca94
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2022-09-28 12:31:50 -0600

    Create biblioteca.sql

---
+++ sql/biblioteca.sql
@@ -0,0 +1,49 @@
+﻿# SQL-Front 5.1  (Build 4.16)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost:3308    Database: biblioteca
+# ------------------------------------------------------
+# Server version 5.5.5-10.1.13-MariaDB
+
+DROP DATABASE IF EXISTS `biblioteca`;
+CREATE DATABASE `biblioteca` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `biblioteca`;
+
+#
+# Source for table libro
+#
+
+DROP TABLE IF EXISTS `libro`;
+CREATE TABLE `libro` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `autor` varchar(255) DEFAULT NULL,
+  `editorial` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Dumping data for table libro
+#
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+INSERT INTO `libro` VALUES (1,'9781591167532','Hunter x Hunter, Vol. 1','Yoshihiro Togashi','VIZ Media LLC');
+INSERT INTO `libro` VALUES (2,'9781421569062','Monster: The Perfect Edition, Vol. 1','Naoki Urasawa','VIZ Media LLC');
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
